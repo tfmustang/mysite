eee9db3b39b49137efe2ace64a805c1aa46b8f58
tfmustang <limuijin@gmail.com>
2018-03-15 18:17:17 +0900

    inon

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'qcrgjg49mf4=8l1akxk)@9(&2udw(w^b@#_jjvv=$i@@lus0yz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True 
 
-ALLOWED_HOSTS = ["192.168.0.12"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
